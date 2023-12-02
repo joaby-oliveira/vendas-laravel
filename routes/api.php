dcaa7618350db0d5449a0a89a605e72ab09bd284
Joaby <joabyoliveira2004@gmail.com>
2023-12-02 15:21:26 -0300

    feat: create Sale resource

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\SaleController;
 use App\Http\Controllers\SalesmanController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/vendedor', SalesmanController::class);
+Route::apiResource('/venda', SaleController::class);
